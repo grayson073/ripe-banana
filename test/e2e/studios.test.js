ed1047930a928081c6693b9d37de5a88dd11e5e4
Mario Quintana <pyrros2000@gmail.com>
2018-07-21 12:31:44 -0700

    getting error connection

--- test/e2e/studios.test.js
+++ test/e2e/studios.test.js
@@ -3,9 +3,11 @@ const request = require('./request');
 const { dropCollection } = require('./db');
 const { checkOk } = request;
 
-describe('Studios API', () => {
+describe.only('Studios API', () => {
 
     beforeEach(() => dropCollection('studios'));
+    beforeEach(() => dropCollection('films'));
+    beforeEach(() => dropCollection('actors'));
 
     function save(studio) {
         return request
@@ -18,15 +20,46 @@ describe('Studios API', () => {
     let univision;
     beforeEach(() => {
         return save({
-            name: 'Univision'  
+            name: 'Univision',
+            address: {
+                city: 'Portland',
+                state: 'Oregon',
+                country: 'USA'
+            }
         })
             .then(data => {
                 univision = data;
             });
     });
 
+    let pacino; 
+    beforeEach(() => {
+        return request
+            .post('api/actors')
+            .send({ name: 'Al Pacino' })
+            .then(({ body }) => pacino = body);
+    });
+
+    let dogDay;
+    beforeEach(() => {
+        return save({
+            title: 'Dog Day',
+            studio: univision._id,
+            released: 1990,
+            cast: [{
+                actor: pacino._id
+            }]
+
+        })
+            .then(data => {
+                dogDay = data;
+            });
+    });
+
+
 
     it('Saves a studio', () => {
+        console.log(univision._id);
         assert.isOk(univision._id);
     });
 
@@ -44,6 +77,20 @@ describe('Studios API', () => {
                 assert.deepEqual(body, [univision, fox]);
             });
     });
+
+    const makeFilm = (film, studio) => {
+        const combined = {
+            _id: film._id,
+            title: film.title,
+            released: film.released
+        };
+        combined.studio = {
+            _id: studio._id,
+            name: studio.name
+        };
+        return combined;
+
+    } ;
 
     it('Gets a studio by id', () => {
         return request
