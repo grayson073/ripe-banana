5fcb7602a0395834e514ea783cee4d86012bbc1f
Mario Quintana <pyrros2000@gmail.com>
2018-07-23 09:46:16 -0700

    review schema model passes validation and required fields

--- test/unit/review.test.js
+++ test/unit/review.test.js
@@ -4,12 +4,37 @@ const Review = require('../../lib/models/review');
 const { getErrors } = require('./_helpers');
 
 describe('Review model', () => {
-    
-    it.skip('Validates good model', () => {
 
+    
+    it('Validates good model', () => {
+        const data = {
+            rating: 5,
+            reviewer: Types.ObjectId(),
+            review: 'It was good',
+            film: Types.ObjectId(),
+            // timestamps: {
+
+            //     createdAt: new Date(),
+            //     updatedAt:  new Date(),
+            // }
+        };
+
+        const review = new Review(data);
+        review.save();
+        console.log('thissssss', review.timestamps);
+        const json = review.toJSON();
+        delete json._id;
+        assert.isUndefined(review.validateSync());
+        
     });
 
-    it.skip('Validates required fields', () => {
+    it('Validates required fields', () => {
+        const review = new Review({});
+        const errors = getErrors(review.validateSync(), 4);
 
+        assert.equal(errors.rating.kind, 'required'); 
+        assert.equal(errors.reviewer.kind, 'required'); 
+        assert.equal(errors.film.kind, 'required'); 
+        assert.equal(errors.review.kind, 'required'); 
     });
 });
