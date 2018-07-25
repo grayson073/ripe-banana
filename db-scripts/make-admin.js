e218af26ba649b40d6d2769202a11ca64e798c92
Kevin Grayson <k.grayson.ms@gmail.com>
2018-07-25 14:34:26 -0700

    Final housekeeping and cleanup

--- db-scripts/make-admin.js
+++ db-scripts/make-admin.js
@@ -3,16 +3,16 @@ const connect = require('../lib/util/connect');
 const Reviewer = require('../lib/models/user');
 const mongoose = require('mongoose');
 
-const MONGODB_URI = process.env.MONGDB_URI || 'mongodb://localhost:27017/pirates';
-connect(MONGDB_URI);
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/pirates';
+connect(MONGODB_URI);
 
 Reviewer.findByIdAndUpdate(
-    /* ObjectId here */,
+    // ObjectId here ,
     {
         $addToSet: {
             roles: 'admin'
         }
     }
 )
-    .catch(console.log('Database error:', err))
+    .catch(console.log('Database error'))
     .then(() => mongoose.connection.close());
