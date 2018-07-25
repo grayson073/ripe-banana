687f669c5cd6f02982fa7f8410d038f1b9bd6047
Kevin Grayson <k.grayson.ms@gmail.com>
2018-07-24 17:04:52 -0700

    Updated reviewer model test successful

--- test/unit/reviewer.test.js
+++ test/unit/reviewer.test.js
@@ -7,22 +7,33 @@ describe('Reviewer model', () => {
     it('Validates good model', () => {
         const data = {
             name: 'Joan Sebastian',
-            company: 'PEMEX'
+            company: 'PEMEX',
+            email: 'email@email.com',
+            password: 'password',
+            roles: []
         };
 
         const reviewer = new Reviewer(data);
 
-        const json = reviewer.toJSON();
-        delete json._id;
-        assert.deepEqual(json, data);
+        assert.equal(reviewer.email, data.email);
+        assert.isUndefined(reviewer.password, 'Password should not be set');
+
+        reviewer.generateHash(data.password);
+        assert.isDefined(reviewer.hash, 'Hash is defined');
+        assert.notEqual(reviewer.hash, data.password, 'Hash is not the same as password');
         assert.isUndefined(reviewer.validateSync());
+
+        assert.isTrue(reviewer.comparePassword(data.password), 'Compare good password');
+        assert.isFalse(reviewer.comparePassword('Bad password'), 'Compare bad password');
     });
 
     it('Requires a reviewer name and company', () => {
         const reviewer = new Reviewer({});
-        const errors = getErrors(reviewer.validateSync(), 2);
+        const errors = getErrors(reviewer.validateSync(), 4);
         assert.equal(errors.name.kind, 'required');
         assert.equal(errors.company.kind, 'required');
+        assert.equal(errors.email.kind, 'required');
+        assert.equal(errors.hash.kind, 'required');
         
     });
 
